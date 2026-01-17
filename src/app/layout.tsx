1bd7e03120ae418b7d51656690faa7518841f9b7
hendras1722 <muhsyahendraa1722@gmail.com>
2026-01-17 18:53:10 +0700

    fix: oi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { QueryProvider } from "@/components/QueryClientProvider";
-import { WebVitals } from "@/components/web-vitals";
+import { Geist, Geist_Mono } from "next/font/google"
+import { QueryProvider } from "@/components/QueryClientProvider"
+import { ThemeProvider } from "@/components/theme-provider"
+import { WebVitals } from "@/components/web-vitals"
+import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+  console.log("aku")
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -35,5 +36,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
